ee8d295849bc8169318dea8cf21f700165c64856
onderadioband <116645502+onderadioband@users.noreply.github.com>
2022-12-06 15:24:20 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@
 // @ts-ignore
 //const tl = gsap.timeline({ defaults: { ease: "power1.out" } })
 
+var images = document.getElementsByClassName("little-img")
+var mainImg = document.getElementById("main-img")
+for (var i = 0; i < images.length; i += 1) {
+  images[i].addEventListener("click", function () {
+    mainImg.style.opacity = 0
+    mainImg.src = this.src
+    gsap.to(mainImg, {
+      duration: 0.5,
+      opacity: 1,
+      ease: Power3.easeOut,
+    })
+  })
+}
+
 gsap.from(".about-content", {
   opacity: 0,
   scale: 0.7,
